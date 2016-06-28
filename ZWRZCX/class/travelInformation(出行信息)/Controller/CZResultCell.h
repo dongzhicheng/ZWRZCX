959d4e8d44da41ad5db52cd1d3eb7f63647c2e02
dongzhicheng <18410080092@163.com>
2016-06-28 11:26:45 +0800

    点击搜索框的时候不要隐藏导航栏

---
+++ ZWRZCX/class/travelInformation(出行信息)/Controller/CZResultCell.h
@@ -0,0 +1,13 @@
+//
+//  CZResultCell.h
+//  ZWRZCX
+//
+//  Created by 董志成 on 16/6/28.
+//  Copyright © 2016年 董志成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CZResultCell : UITableViewCell
+
+@end
---
+++ ZWRZCX/class/travelInformation(出行信息)/Controller/CZResultCell.m
@@ -0,0 +1,24 @@
+//
+//  CZResultCell.m
+//  ZWRZCX
+//
+//  Created by 董志成 on 16/6/28.
+//  Copyright © 2016年 董志成. All rights reserved.
+//
+
+#import "CZResultCell.h"
+
+@implementation CZResultCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
